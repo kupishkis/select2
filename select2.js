5b723087ed00206d12be316f9b55514c86a8eb2a
Paulius Dambravičius <dambravicius@gmail.com>
2015-03-30 10:33:37 +0300

    Revert "CALLCENTER-4839: Select2 element performs AJAX queries to Leos.Address"

--- select2.js
+++ select2.js
@@ -2412,7 +2412,7 @@ the specific language governing permissions and limitations under the Apache Lic
 
             // and highlight it
             if (noHighlightUpdate !== false) {
-                if (selected >= 0) {
+                if (initial === true && selected >= 0) {
                     this.highlight(selected);
                 } else {
                     this.highlight(0);
